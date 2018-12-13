cd93b5da20b502b447297e0d8477f57fea296d08
Mustafa Muhammad Saad <eng.mustafa.muhammad@gmail.com>
2018-12-14 00:20:05 +0300

    Startup

--- TestButtonAction/ViewController.swift
+++ TestButtonAction/ViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    var label:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //Create The first Label
+        let label = UILabel()
+        label.frame = CGRect(x: 40, y: 40, width: 40, height: 40)
+        label.text = "\(count)"
+        view.addSubview(label)
+        self.label = label
+        
+        //Create the first button
+        let button = UIButton()
+        button.setTitle("Click Me", for: .normal)
+        button.frame = CGRect(x: 40, y: 60, width: 100, height: 100)
+        button.setTitleColor(UIColor.red, for: .normal)
+        button.addTarget(self, action: #selector(ViewController.incrementCount), for: .touchUpInside)
+        view.addSubview(button)
+    }
+    
+    @objc func incrementCount(){
+        count += 1
+        self.label.text = "\(count)"
     }
 
 
